ec64c401232c624caf11d85f7fe65207d51bf0eb
devRenatoRodrigues <rehrlz@hotmail.com>
2023-09-21 17:23:18 -0300

    Task Two Complete

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,22 @@
+import pytest
+
+from challenges.challenge_encrypt_message import encrypt_message
+
 # from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError) as type_error_key:
+        encrypt_message("test", "re")
+
+    assert str(type_error_key.value) == "tipo inválido para key"
+
+    with pytest.raises(TypeError) as type_error_message:
+        encrypt_message(123234, 3)
+
+    assert str(type_error_message.value) == "tipo inválido para message"
+
+    assert encrypt_message("teste", 5) == "etset"
+    assert encrypt_message("teste", 2) == "ets_et"
+    assert encrypt_message("teste", 3) == "set_et"
+    assert encrypt_message("hello", 8) == "olleh"
